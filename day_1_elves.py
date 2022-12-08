e501dad58d0d57b52a13aaa83cec2b3561c000b0
Jared Frees <77975450+jaredfrees-pg@users.noreply.github.com>
2022-12-08 18:07:34 -0500

    Add files via upload

---
+++ day_1_elves.py
@@ -0,0 +1,16 @@
+
+
+# each item is total calories for one elf
+elf_calorie_list  = list()
+with open('input.txt') as file:
+    one_elf_calories = 0
+    for line in file:
+        # print(line)
+        
+        if line.strip() == '':
+            elf_calorie_list.append(one_elf_calories)
+            one_elf_calories = 0
+        else:
+            one_elf_calories += int(line)
+
+print(max(elf_calorie_list))
